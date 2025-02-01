6a6b3163acc830bea4bc8306086dfe076d93e6d3
USER <kevinpb.py@gmail.com>
2025-02-01 19:23:46 +0100

    create an ui or view for thask

---
+++ src/taskui.py
@@ -0,0 +1,37 @@
+class UITask:
+
+    def __init__(self, db):
+        self.db = db
+        pass
+
+    def showDataList(self, df):
+        print("=======================")
+        print("Index", "name", "time")
+        print("=======================")
+        for index, row in df.iterrows():
+            print(index, row["name"], row["time"])
+            print("......................")
+
+    def add(self):
+        inputName = input("Enter the task name: ")
+        inputData = {"name": inputName, "time": "1d"}
+
+        dbPath = self.db.pathTo(destination="../db", origin=__file__)
+
+        tmp = self.db.addFile(
+            data=inputData,
+            fileName=dbPath + "/tasks-list",
+            format="json",
+            columns=["name", "time"],
+        )
+
+        self.showDataList(tmp)
+
+    def show(self):
+        pass
+
+    def dell(self):
+        pass
+
+    def dellAll(self):
+        pass
