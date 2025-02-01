cded3db5b1b0d1948599f8d5253488620e401a01
USER <kevinpb.py@gmail.com>
2025-02-01 17:57:42 +0100

    add addFile method

--- src/lib/file.py
+++ src/lib/file.py
@@ -2,14 +2,16 @@ import pandas as pd
 from pathlib import Path
 
 
-def readFile(fileName="tasks-list", format="json"):
-    file = fileName + "." + format
-    if format == "json":
-        list = pd.read_json(file, orient="records")
-    if format == "csv":
-        list = pd.read_csv(file)
-    return list
-
+def readFile(fileName="tasks-list", format="json", columns=["name", "value"]):
+    try:
+        file = fileName + "." + format
+        if format == "json":
+            dataFrame = pd.read_json(file, orient="records")
+        if format == "csv":
+            dataFrame = pd.read_csv(file)
+        return dataFrame
+    except Exception:
+        return pd.DataFrame([], columns=columns)
 
 def writeFile(data, fileName="tmp", format="json"):
     file = fileName + "." + format
@@ -18,7 +20,12 @@ def writeFile(data, fileName="tmp", format="json"):
         dataFrame.to_json(file, orient="records")
     if format == "csv":
         dataFrame.to_csv(file)
+    return dataFrame
 
+def addFile(data, fileName="tmp", format="json", columns=["name", "value"]):
+    df = readFile(fileName, format, columns)
+    df.loc[len(df)] = data
+    return writeFile(df, fileName, format)
 
 def pathTo(destination, origin=__file__):
     pathORG = Path(origin).resolve().parent
